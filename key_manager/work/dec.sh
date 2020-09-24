514154a88f42de7e3e6d19a1cac063a73338c389
ads-iwase-yuta <iwase.yuta@adniss.jp>
2020-09-24 16:00:18 +0900

    メッセージ追加

--- key_manager/work/dec.sh
+++ key_manager/work/dec.sh
@@ -1,1 +1,2 @@
-openssl aes-256-cbc -d -iter 100 -in encrypted
+openssl aes-256-cbc -d -iter 100 -in encrypted &&
+echo "NOTICE: If have checked your secret text, should run this command 'cls' to clean the console."
